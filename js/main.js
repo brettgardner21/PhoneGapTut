e49266fdb69d6f6c9b849fc1dbd850f7a1d20cec
Brett Gardner <brett.gardner@eloqua.com>
2012-12-01 15:00:33 -0500

    notifications

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+
+    
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     },
 
-    initialize: function() {
-        this.store = new LocalStorageStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
 
-};
+
+    initialize: function() {
+        var self = this;
+
+        this.store = new MemoryStore(function() {
+            self.showAlert("test","test");
+            $('body').html(new HomeView(self.store).render().el);
+        });
+
+    }
+}
 
 app.initialize();
